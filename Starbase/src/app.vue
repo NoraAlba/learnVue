51c05da7f072c970c1d93f7cbe7f23a3031c7be2
NoraAlba <35965319+NoraAlba@users.noreply.github.com>
2018-06-13 09:53:08 +0100

    Add files via upload

---
+++ Starbase/src/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h3>{{title}}</h3>
+<button class="btn btn-primary" @click="fetchCharacter">Test Fetch char</button>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      title: 'Generate Your Collection'
+    }
+  },
+  methods: {
+    fetchCharacter() {
+      fetch('http://swapi.co/api/people',{
+          method: 'GET',
+        mode:"no-cors"
+      }).then(response => console.log(response))
+    }
+  }
+}
+
+</script>
